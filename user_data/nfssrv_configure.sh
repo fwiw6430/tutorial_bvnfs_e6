2a950133181a1835054717915d7930079262c120
Tsutomu Miyashita <tsutomu.miyashita@oracle.com>
2024-10-01 17:38:45 +0900

    Initial release

--- user_data/nfssrv_configure.sh
+++ user_data/nfssrv_configure.sh
@@ -17,14 +17,20 @@ for ((i = 0; i < ${#ar_bv_dev_name[@]}; i++))
   done
 }
 
+echo "Start creating volume group"
 sudo vgcreate bv $bv_dev_list
+echo "Start creating logical volume"
 sudo lvcreate -y -l 100%FREE -n bv bv
+echo "Start formatting XFS file system"
 sudo mkfs.xfs -L blockvolume /dev/bv/bv
 echo "LABEL=blockvolume /mnt/bv/ xfs defaults,noatime 0 0" | sudo tee -a /etc/fstab
 sudo systemctl daemon-reload
 sudo mkdir -p /mnt/bv
+echo "Start mounting file system"
 sudo mount /mnt/bv
+echo "Start configuring secondary NIC"
 sudo oci-network-config configure
 echo "/mnt/bv 10.0.2.0/24(rw,sync,no_root_squash)" | sudo tee -a /etc/exports
 sudo sed -i 's/# threads=8/threads=64/g' /etc/nfs.conf
+echo "Start NFS server"
 sudo systemctl enable --now nfs-server rpcbind
